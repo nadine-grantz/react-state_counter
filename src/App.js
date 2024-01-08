c7f6f387d47666c748467136e1e5703e32cb4518
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-08 16:36:02 +0100

    add first try

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  //let count = 0;
+  const [number, setNumber] = useState(0);
+
+  function increase() {
+    setNumber(number + 1);
+  }
+
+  const descrease = () => {
+    setNumber(number - 1);
+  };
 
   return (
-    <div className="container">
-      <h1>{count}</h1>
-      <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
-          -
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
-          +
-        </button>
+    <div>
+      <div className="container">
+        <h1>{number}</h1>
+        <div className="button-container">
+          <button type="button" onClick={increase}>
+            -
+          </button>
+          <button type="button" onClick={(descrease, console.log("🤔"))}>
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
